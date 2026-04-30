afc1a6d983356dddff1fa6d3f9f3a198992084db
krzys <17764528+wegrzyns@users.noreply.github.com>
2026-04-30 12:44:51 +0200

    Anagram

---
+++ src/main/java/com/example/demo/neetcode/top150/Anagram.java
@@ -0,0 +1,48 @@
+package com.example.demo.neetcode.top150;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+/*
+Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+Example 1:
+
+Input: s = "racecar", t = "carrace"
+
+Output: true
+Example 2:
+
+Input: s = "jar", t = "jam"
+
+Output: false
+Constraints:
+
+s and t consist of lowercase English letters.
+ */
+public class Anagram {
+
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> characterOccurrences = new HashMap<>();
+        if (s.length() != t.length()) return false;
+
+        traverseString(s, characterOccurrences, 1);
+        traverseString(t, characterOccurrences, -1);
+
+        return  characterOccurrences.isEmpty();
+    }
+
+    private static void traverseString(String input, Map<Character, Integer> characterOccurrences, int counterUpdate) {
+        for (int i = 0; i < input.length(); i++) {
+            char character = input.charAt(i);
+            characterOccurrences.merge(character, counterUpdate, (currentValue, increment) -> {
+                int remainingOccurrences = currentValue + increment;
+                if (remainingOccurrences == 0) return null;
+                return remainingOccurrences;
+            });
+        }
+    }
+}
---
+++ src/test/java/com/example/demo/neetcode/top150/AnagramTest.java
@@ -0,0 +1,58 @@
+package com.example.demo.neetcode.top150;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+Example 1:
+
+Input: s = "racecar", t = "carrace"
+
+Output: true
+Example 2:
+
+Input: s = "jar", t = "jam"
+
+Output: false
+ */
+class AnagramTest {
+
+    private Anagram anagram;
+
+    @BeforeEach
+    public void setup() {
+        anagram = new Anagram();
+    }
+
+    @ParameterizedTest
+    @EnumSource(TestCase.class)
+    public void test(TestCase testCase) {
+        // given
+
+        // when
+        boolean result = anagram.isAnagram(testCase.string1, testCase.string2);
+
+        // then
+        assertThat(result).isEqualTo(testCase.isAnagram);
+
+    }
+
+    enum TestCase {
+        CASE_1("racecar", "carrace", true),
+        CASE_2("jar", "jam", false);
+
+        private String string1;
+        private String string2;
+        private boolean isAnagram;
+
+        TestCase(String string1, String string2, boolean isAnagram) {
+            this.string1 = string1;
+            this.string2 = string2;
+            this.isAnagram = isAnagram;
+        }
+    }
+
+}
